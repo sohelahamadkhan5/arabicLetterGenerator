e4f3df2d0a55e624099347f2d6bf26e21ff66597
Sohel Ahmed Khan <sohelahamadkhan5@gmail.com>
2022-08-10 23:12:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Random Arabic Letter generator
+//Math.floor((Math.random()*10)+6)  <--------for (0 to 15)
+//Math.floor((Math.random()*10)+1)  <--------for (0 to 10)
+
+//
+
+
+
+
+/* Arabic Letter Array */
+let ArabicLetters = ["ا","ب","ت","ث","ج","ح","خ","د","ذ","ر","ز","س","ش","ص","ض","ط","ظ","ع","غ","ف","ق","ك","ل","م","ن","ه","و","ي","ء"];
+
+
+
+
+//Current Year
+let currentYear = new Date().getFullYear();
+console.log(currentYear); 
+
+//jQuery
+$("#year").html(currentYear); 
